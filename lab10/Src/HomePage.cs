767a2b0374be94289df7804bde9e8fb850c4e653
Toshi <96656441+tartagliaimpact@users.noreply.github.com>
2024-09-20 20:38:17 +0300

    Add files via upload

---
+++ lab10/Src/HomePage.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.PageObjects;
+using NUnit.Framework.Legacy;
+using SeleniumExtras.WaitHelpers;
+using OpenQA.Selenium.Interactions;
+
+namespace lab10.Src
+{
+    class HomePage
+    {
+        String homePageUrl = "https://shikimori.one/";
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+
+        public HomePage(IWebDriver driver)
+        {
+            this.driver = driver;
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            driver.Url = homePageUrl;
+            PageFactory.InitElements(driver, this);
+        }
+
+
+        public IWebElement elementHeader { get; set; }
+        public IWebElement elementHeaderSearchDropDownMenuCategoryManga { get; set; }
+        public IWebElement elementHeaderSearchInput { get; set; }
+
+        public IWebElement elementDropDownMenu { get; set; }
+        public IWebElement elementCalendarCategory {  get; set; }
+
+        String XpathHeader = "//*[@id=\"dashboards_show\"]/header";
+        String XpathHeaderSearchDropDownMenuCategoryManga = "//*[@id=\"dashboards_show\"]/header/div[3]/div/div/div/div[2]";
+        String XpathHeaderSearchInput = "//*[@id=\"dashboards_show\"]/header/div[3]/label/input";
+
+
+        String XpathHeaderDropDownMenu = "//*[@id=\"dashboards_show\"]/header/div[1]/div/div";
+        String XpathHeaderCalendarCategory = "//*[@id=\"dashboards_show\"]/header/div[1]/div/div/a[11]";
+
+
+
+        public void clickHeader()
+        {
+            elementHeader = wait.Until(ExpectedConditions.ElementToBeClickable(
+                By.XPath(XpathHeader)));
+            elementHeader.Click();
+        }
+
+        public void clickCategory()
+        {
+            elementHeaderSearchDropDownMenuCategoryManga = wait.Until(ExpectedConditions.ElementToBeClickable(
+                By.XPath(XpathHeaderSearchDropDownMenuCategoryManga)));
+            elementHeaderSearchDropDownMenuCategoryManga.Click();
+        }
+
+        public IWebDriver searchInHomePage(string inputSearch)
+        {
+            elementHeaderSearchInput = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath(XpathHeaderSearchInput)));
+            elementHeaderSearchInput.SendKeys(inputSearch);
+            elementHeaderSearchInput.SendKeys(Keys.Enter);
+            return driver;
+        }
+
+        public void changeHeaderClass()
+        {
+            elementHeader = driver.FindElement(By.XPath(XpathHeader));
+            IJavaScriptExecutor mainHeaderJs = (IJavaScriptExecutor)driver;
+            mainHeaderJs.ExecuteScript("arguments[0].className = 'l-top_menu-v2 is-submenu';", elementHeader);
+        }
+
+        public void changeMenuClass()
+        {
+            elementDropDownMenu = driver.FindElement(By.XPath(XpathHeaderDropDownMenu));
+            IJavaScriptExecutor mainPagebuttonJs = (IJavaScriptExecutor)driver;
+            mainPagebuttonJs.ExecuteScript("arguments[0].className = 'menu-dropdown main active';", elementDropDownMenu);
+        }
+
+        public IWebDriver ClickMenuCategory()
+        {
+            elementCalendarCategory = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(XpathHeaderCalendarCategory)));
+            new Actions(driver).MoveToElement(elementCalendarCategory).Perform();
+            elementCalendarCategory.Click();
+            return driver;
+        }
+
+
+    }
+}
+
+
---
+++ lab10/Test/TestClass.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using lab10.Src;
+
+namespace lab10.Test
+{
+    class TestClass
+    {
+
+
+        IWebDriver driver;
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver("D:\\институт\\6-й семестр\\Тестирование программного обеспечения\\lab10\\packages\\Selenium.WebDriver.ChromeDriver.129.0.6668.5800\\driver\\win32");
+            driver.Manage().Window.Maximize();
+        }
+
+
+        [Test]
+        public void SearchMangaByName()
+        {
+            HomePage homePage = new HomePage(driver);
+            homePage.clickHeader();
+            homePage.clickCategory();
+            IWebDriver searchPage = homePage.searchInHomePage("Стальной алхимик");
+            ClassicAssert.AreEqual("https://shikimori.one/mangas?search=%D0%A1%D1%82%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D0%B9%20%D0%B0%D0%BB%D1%85%D0%B8%D0%BC%D0%B8%D0%BA",
+                searchPage.Url, "URL страницы не совпадает с ожидаемым.");
+        }
+
+        [Test]
+        public void GetOngoingList()
+        {
+            HomePage homePage = new HomePage(driver);
+            homePage.changeHeaderClass();
+            homePage.changeMenuClass();
+            IWebDriver ongoingPage = homePage.ClickMenuCategory();
+            ClassicAssert.AreEqual("https://shikimori.one/ongoings", ongoingPage.Url, "URL страницы не совпадает с ожидаемым.");
+        }
+
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+
+    }
+}
